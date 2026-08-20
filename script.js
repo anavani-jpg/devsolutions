5d63139cf34bd78a2e6f3a9f2349d4ecdd128985
anavani-jpg <ana.vani@edu.senai.br>
2026-08-20 13:59:44 -0300

    Adiciona seção de contato e validação com JS

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementosById("form-contato").addEventListener("submit" , function(evento) {
+    evento.preventDefault();
+
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+
+    if (nome === "" || email === "") {
+        alert("Por favor, preencha todos os campos. "); 
+    } else {
+      alert("Mensgaem enviada com sucesso!");  
+    }
+});
